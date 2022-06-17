e756300eaa644e7e3dc1fd26e1bb0fff45610e00
marisolGarcia2611 <71054456+marisolGarcia2611@users.noreply.github.com>
2022-06-16 22:43:12 -0500

    botones

--- src/Main.js
+++ src/Main.js
@@ -196,7 +196,7 @@ function Main() {
                                                     <div className=" position-absolute end-0">
                                                         <button className=" btn btn-danger btn__rounded">
                                                         <svg xmlns="http://www.w3.org/2000/svg"  fill="currentColor" className="img__ico bi bi-trash-fill" viewBox="0 0 16 16">
-                                                            <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"/>
+                                                        <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8 2.146 2.854Z"/>
                                                         </svg>    
                                                         </button>
                                                         <button className=" btn btn-dark btn__rounded"><img className="img__ico" src={edit__ico}></img></button>
@@ -373,7 +373,7 @@ function Main() {
 export default Main;
 
 window.onscroll = function() {
-    console.log(window.scrollY);
+    //console.log(window.scrollY);
     if (window.scrollY >= 571){
       document.getElementById("space2").style.position = "fixed";
       document.getElementById("space2").style.top = "0%";
