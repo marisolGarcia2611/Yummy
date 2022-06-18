8fee25a879bb71086a22910a41a18de91fbfb9fd
marisolGarcia2611 <71054456+marisolGarcia2611@users.noreply.github.com>
2022-06-17 19:53:32 -0500

    navbar

--- src/Orden.js
+++ src/Orden.js
@@ -14,12 +14,38 @@ function Orden() {
      return (
        <section>
            <Navbar/>
-           <div className="container px-4 py-5 px-md-5 text-center text-lg-start my-5">
+           <div className="row">
+
+            <div className="col-md-3">
+              <div class="w3-sidebar w3-bar-block tanSide">
+              <a href="#" class="w3-bar-item w3-button">Link 1</a>
+              <a href="#" class="w3-bar-item w3-button">Link 2</a>
+              <a href="#" class="w3-bar-item w3-button">Link 3</a>
+            </div>
+
+              <div className="margSide">
+              ... page content ...
+              </div>
+            </div>
+
+
+            <div className="col-md-8 container px-4 py-5 px-md-5 text-center text-lg-start my-5">
             <h1>Orden</h1>
-         </div>
+           </div>
+           </div>
+           
+
 
        </section>
      
     );
   }
 export default Orden;
+
+function w3_open() {
+  document.getElementById("mySidebar").style.display = "block";
+}
+
+function w3_close() {
+  document.getElementById("mySidebar").style.display = "none";
+}
