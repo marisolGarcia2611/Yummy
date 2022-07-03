a6c8ee9e62c4694bbe1a02a55c8f84b712e34556
marisolGarcia2611 <71054456+marisolGarcia2611@users.noreply.github.com>
2022-07-03 02:39:47 -0500

    elementos añadidos a la pantalla de ordenes

--- src/Orden.js
+++ src/Orden.js
@@ -17,20 +17,161 @@ function Orden() {
      return (
        <section>
            <Navbar/>
-           <div className="">
+           <div>
             <div className="row">
+
               <div className="col-md-4 b-yellow">
-
                 <Search/>
-
-                <div>
-                  <Tarject/>
-                  <Tarject/>
-                </div>
+                  <div>
+                    <Tarject/>
+                    <Tarject/>
+                  </div>
               </div>
+
               <div className="space__stuct col-md-8">
-                <h3 className="letters__struct2">Estilo del cafe</h3>
-                <h1 className="letters__struct">Nombre del cafe</h1>
+                <h3 className="letters__struct2">BEBIDA CALIENTE</h3>
+                <h1 className="letters__struct">Mocha</h1>
+                  <br/>
+
+                <div className="row">
+                  <div className="col-md-8">
+
+                    {/* TAMAÑO */}
+                    <div>
+                    <div>
+                      <h4 className="fw-light letters__struct3">TAMAÑO</h4>
+                      <hr className="letters__struct3"/>
+                    </div>
+              
+                    <div className="row text-center">
+                      <div className="btn tarject__btn col-md-2">
+                          <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="currentColor" class="bi bi-cup" viewBox="0 0 16 16">
+                            <path d="M1 2a1 1 0 0 1 1-1h11a1 1 0 0 1 1 1v1h.5A1.5 1.5 0 0 1 16 4.5v7a1.5 1.5 0 0 1-1.5 1.5h-.55a2.5 2.5 0 0 1-2.45 2h-8A2.5 2.5 0 0 1 1 12.5V2zm13 10h.5a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.5-.5H14v8zM13 2H2v10.5A1.5 1.5 0 0 0 3.5 14h8a1.5 1.5 0 0 0 1.5-1.5V2z"/>
+                          </svg>
+                          <p className="fw-bold lt">CH<br/><p className="fw-light">$00.00</p></p>
+
+                      </div>
+
+                      <div className="btn tarject__btn col-md-2">
+                          <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="currentColor" class="bi bi-cup" viewBox="0 0 16 16">
+                            <path d="M1 2a1 1 0 0 1 1-1h11a1 1 0 0 1 1 1v1h.5A1.5 1.5 0 0 1 16 4.5v7a1.5 1.5 0 0 1-1.5 1.5h-.55a2.5 2.5 0 0 1-2.45 2h-8A2.5 2.5 0 0 1 1 12.5V2zm13 10h.5a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.5-.5H14v8zM13 2H2v10.5A1.5 1.5 0 0 0 3.5 14h8a1.5 1.5 0 0 0 1.5-1.5V2z"/>
+                          </svg>
+                          <p className="fw-bold lt">MD<br/><p className="fw-light">$00.00</p></p>
+
+                      </div>
+
+                      <div className="btn tarject__btn col-md-2">
+                          <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="currentColor" class="bi bi-cup" viewBox="0 0 16 16">
+                            <path d="M1 2a1 1 0 0 1 1-1h11a1 1 0 0 1 1 1v1h.5A1.5 1.5 0 0 1 16 4.5v7a1.5 1.5 0 0 1-1.5 1.5h-.55a2.5 2.5 0 0 1-2.45 2h-8A2.5 2.5 0 0 1 1 12.5V2zm13 10h.5a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.5-.5H14v8zM13 2H2v10.5A1.5 1.5 0 0 0 3.5 14h8a1.5 1.5 0 0 0 1.5-1.5V2z"/>
+                          </svg>
+                          <p className="fw-bold lt">GR<br/><p className="fw-light">$00.00</p></p>
+
+                      </div>
+                    </div>
+
+                  </div>
+
+                    {/* TIPO DE LECHE*/}
+                  <div>
+                    <div>
+                      <br/>
+                      <h4 className="fw-light letters__struct3">LECHE</h4>
+                      <hr className="letters__struct3"/>
+                    </div>
+                    
+
+                    <div className="row text-center">
+                      <div className="btn tarject__btn col-md-3">                          
+                          <p className="fw-bold lt">ENTERA<br/><p className="fw-light">$00.00</p></p>
+
+                      </div>
+
+                      <div className="btn tarject__btn col-md-3">
+                          <p className="fw-bold lt">DESLACTOSADA<br/><p className="fw-light">$00.00</p></p>
+
+                      </div>
+
+                      <div className="btn tarject__btn col-md-3">
+                          <p className="fw-bold lt">LIGHT<br/><p className="fw-light">$00.00</p></p>
+
+                      </div>
+                    </div>
+                    
+                  </div>
+
+                  {/* COMPLEMENTOS */}
+                  <div>
+                    <div>
+                      <br/>
+                      <h4 className="fw-light letters__struct3">COMPLEMENTOS</h4>
+                      <hr className="letters__struct3"/>
+                    </div>
+                    
+
+                    <div className="row text-center">
+                      <div className="btn tarject__btn col-md-2">                          
+                          <p className="fw-bold lt">CHOCOLATE<br/><p className="fw-light">$00.00</p></p>
+
+                      </div>
+
+                      <div className="btn tarject__btn col-md-2">
+                          <p className="fw-bold lt">MALVADISCO<br/><p className="fw-light">$00.00</p></p>
+
+                      </div>
+
+                      <div className="btn tarject__btn col-md-2">
+                          <p className="fw-bold lt">GRANOLA<br/><p className="fw-light">$00.00</p></p>
+
+                      </div>
+
+                      <div className="btn tarject__btn col-md-2">
+                          <p className="fw-bold lt">FRESA<br/><p className="fw-light">$00.00</p></p>
+
+                      </div>
+
+                    </div>
+                    
+                  </div>
+
+                  {/* NOMBRE DEL VASO */}
+                  <div>
+                    <div>
+                      <br/>
+                      <h4 className="fw-light letters__struct3">NOMBRE EN VASO</h4>
+                      <hr className="letters__struct3"/>
+                    </div>
+                    
+
+                    <div className="row text-center">
+                      <div className="btn tarject__btn col-md-10">   
+                          <p className="text-start fw-light">Personaliza tu vaso con tu nombre</p>
+                          <div className="row">
+                            <div className="col-md-10">
+                            <input className="form-control inp" type="text"  placeholder="name" name="name"/>
+                            </div>
+                            <div className="col-md-2">
+                            <button className="btn btn__struct">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-plus-circle" viewBox="0 0 16 16">
+                              <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+                              <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
+                            </svg></button>
+                            </div>                         
+                          </div>                       
+                          
+                      </div>
+                    </div>
+                    
+                  </div>
+
+                  </div>
+
+                  <div className="col-md-4">
+                    <div className="space__stuct1">
+                      hola
+                    </div>
+                  </div>
+
+                </div>
               </div>
 
             </div>
